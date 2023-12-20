969fbca9bdbe2924ca68ef35f2e9550bc7ffb6e7
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-20 12:20:13 -0800

    Update konoha-deploy.tf

--- konoha-deploy.tf
+++ konoha-deploy.tf
@@ -29,7 +29,7 @@ resource "kubernetes_deployment_v1" "konoha-deploy" {
 
       spec {
         container {
-          image = "princebabs/konoha:8"
+          image = "princebabs/konoha"
           name  = "konoha-pod"
 
           port {
