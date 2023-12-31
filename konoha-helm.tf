5a6c6ef864b1d8b0f867eca9f7ce457286dc1dc5
Babs <68171102+Ohubabs@users.noreply.github.com>
2023-12-30 21:11:37 -0800

    Update konoha-helm.tf

--- konoha-helm.tf
+++ konoha-helm.tf
@@ -1,11 +1,11 @@
-resource "helm_release" "konoha-app1" {
-  name       = "konoha1"
-  namespace = "default"
-  chart      = "konoha"
-  repository = "https://konoha-23.github.io/landmark/"
-  
-  values = [
-    "${file("konoha.yml")}"
-  ]
-
-}
+#resource "helm_release" "konoha-app1" {
+#  name       = "konoha1"
+#  namespace = "default"
+#  chart      = "konoha"
+#  repository = "https://konoha-23.github.io/landmark/"
+#  
+#  values = [
+#    "${file("konoha.yml")}"
+#  ]
+#
+#}
